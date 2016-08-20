b9d42cb5df5113a8c2f6a8d9b4071698f1761fac
Danyal Raza <smdraza@uwaterloo.ca>
2016-08-20 19:45:17 -0400

    create movement

---
+++ Assets/Main/Scenes/move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+
+	private Vector3 moveDirection = Vector3.zero;
+	public float gravity = 20f;
+	private CharacterController controller;
+
+	public float JumpSpeed = 8.0f;
+	public float Speed = 6.0f;
+//	public Transform CharacterGO;
+
+	bool isInSwipeArea;
+	// Use this for initialization
+	void Start () {
+		controller = (CharacterController) GetComponent(typeof(CharacterController));
+		moveDirection = transform.forward;
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= Speed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		moveDirection.y -= gravity * Time.deltaTime;
+		//move the player
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+	private void CheckHeight()
+	{
+//		if (transform.position.y < -10)
+//		{
+//			GameManager.Instance.Die();
+//		}
+	}
+}
