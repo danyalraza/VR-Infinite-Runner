114023230e56c5ad2758bdf198377e7bf2f3bea0
Ian Lo <ianlo196@gmail.com>
2016-08-21 12:12:37 -0400

    Added speedup and music

--- Assets/Main/Scripts/TileManager.cs
+++ Assets/Main/Scripts/TileManager.cs
@@ -8,23 +8,12 @@ public class TileManager : MonoBehaviour {
 	public float currentpos;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 30; i++) {
 			SpawnChunk ();
 		}
 		currentpos = 0;
 	}
-
-	// Update is called once per frame
-	void FixedUpdate () {
-		GameObject playerObject = GameObject.Find("Tank");
-		Vector3 playerPos = playerObject.transform.position;
-		currentpos += playerObject.GetComponent<Rigidbody>().velocity.z * Time.fixedDeltaTime;
-		if (currentpos > 50) {
-			SpawnChunk();
-			currentpos = 0;
-		}
-	}
-
+		
 	public void SpawnChunk() {
 		int randomTileIndex = Random.Range (0, levelTiles.Length);
 		nextChunkPrefab = levelTiles [randomTileIndex];
