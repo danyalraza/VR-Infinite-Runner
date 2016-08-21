36d7c0666bc3dc0450f49bab6e83713b6559e941
Alston Lin <alstonlin812@gmail.com>
2016-08-20 22:14:57 -0400

    Jumping works!

--- Assets/Main/Scripts/NetworkController.cs
+++ Assets/Main/Scripts/NetworkController.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class NetworkController : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
--- Assets/Main/Scripts/move.cs
+++ Assets/Main/Scripts/move.cs
@@ -3,28 +3,49 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 
+	private const float ACCELERATION = 2f;
+	private const float GRAVITY = 0.981f;
+	private const float FORWARD_SPEED = 6.0f;
 
-	private Vector3 moveDirection = Vector3.zero;
-	public float gravity = 20f;
+	private Vector3 yUnitVec = new Vector3 (0, 1, 0);
+	private Vector3 forward = Vector3.zero;
 	private CharacterController controller;
+	private float ySpeed = 0;
+	private bool pressed = false;
 
-	public float JumpSpeed = 8.0f;
-	public float Speed = 6.0f;
-//	public Transform CharacterGO;
-
-	bool isInSwipeArea;
-	// Use this for initialization
 	void Start () {
 		controller = (CharacterController) GetComponent(typeof(CharacterController));
-		moveDirection = transform.forward;
-		moveDirection = transform.TransformDirection(moveDirection);
-		moveDirection *= Speed;
+		forward = transform.forward;
+		forward = transform.TransformDirection(forward);
+		forward *= FORWARD_SPEED;
 	}
 
-	// Update is called once per frame
 	void Update () {
-		controller.Move(moveDirection * Time.deltaTime);
+		Vector3 dir = Vector3.zero;
+		dir += forward;
+		// Applies y acceleration
+		if (isPressed()) {
+			ySpeed += ACCELERATION;
+		}
+		if (controller.isGrounded && ySpeed < 0) {
+			ySpeed = 0;
+		} else {
+			ySpeed -= GRAVITY;
+		}
+		dir += ySpeed * yUnitVec;
+		controller.Move(dir * Time.deltaTime);
 	}
+
+	private bool isPressed(){
+		// Checks for updates
+		if (Input.GetMouseButtonDown(0)) {
+			pressed = true;
+		} else if (Input.GetMouseButtonUp(0)) {
+			pressed = false;
+		}
+		return GvrViewer.Instance.Triggered || pressed;
+	}
+
 	private void CheckHeight()
 	{
 //		if (transform.position.y < -10)
