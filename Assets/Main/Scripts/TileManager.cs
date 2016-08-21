6e53e367f158b9b358108bdf4776e75f71615938
Ian Lo <ianlo196@gmail.com>
2016-08-21 01:12:02 -0400

    Generating many chunks now.

---
+++ Assets/Main/Scripts/TileManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+	public GameObject nextChunkPrefab;
+	public GameObject currentChunk;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			SpawnChunk ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnChunk() {
+		currentChunk = (GameObject) Instantiate (nextChunkPrefab, currentChunk.transform.GetChild(0).position, Quaternion.identity);
+	}
+}
