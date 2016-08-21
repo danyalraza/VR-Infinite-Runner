48890a4c766c7543bcad1b45f1df46650d4aeb45
Danyal Raza <smdraza@uwaterloo.ca>
2016-08-21 03:04:22 -0400

    add block creation

--- Assets/Main/Scripts/ChunkController.cs
+++ Assets/Main/Scripts/ChunkController.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class ChunkController : MonoBehaviour {
-	public GameObject thing;
+	public GameObject playerObject;
+	public float playerPos;
 	// Use this for initialization
 	void Start () {
-		
+		playerObject = GameObject.Find("Tank");
+		playerPos = playerObject.transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		playerPos = playerObject.transform.position.z;
+		if (playerPos > (gameObject.transform.position.z + 50f)) {
+			Destroy(gameObject);
+		}
 	}
 }
--- Assets/Main/Scripts/TileManager.cs
+++ Assets/Main/Scripts/TileManager.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class TileManager : MonoBehaviour {
 	public GameObject nextChunkPrefab;
 	public GameObject currentChunk;
+	public float currentpos;
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < 10; i++) {
 			SpawnChunk ();
 		}
+		currentpos = 0;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		GameObject playerObject = GameObject.Find("Tank");
+		Vector3 playerPos = playerObject.transform.position;
+		currentpos += playerObject.GetComponent<Rigidbody>().velocity.z * Time.fixedDeltaTime;
+		Debug.Log ("The current velocity is: " + playerObject.GetComponent<Rigidbody> ().velocity.z);
+
+		Debug.Log ("The current position is: " + currentpos);
+		if (currentpos > 50) {
+			SpawnChunk();
+			currentpos = 0;
+		}
 	}
 
 	public void SpawnChunk() {
